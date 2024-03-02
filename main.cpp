2b247f20ae13224ac015a691b67fc089b21b26fe
dummy-decoy <79310325+dummy-decoy@users.noreply.github.com>
2024-03-02 10:23:45 +0100

    more stuff (#5) (#6)
    - better error handling
    - differentiate between variables and constants
    - added function calls

--- main.cpp
+++ main.cpp
@@ -2,18 +2,20 @@
 #include <cstdint>
 #include <cmath>
 #include <cctype>
+#include <functional>
 #include <stdexcept>
 #include <map>
 #include <string>
+#include <vector>
 #include <iostream>
-
 
 /*
 digit = ('0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9')
 letter = ('a'..'z'|'A'..'Z'|'_')
 
 number ::= digit+ ('.' digit+)? ('e' ('+'|'-')? digit+)?
-identifier ::= letter+ (digit|letter)*
+name ::= letter+ (digit|letter)*
+identifier ::= name ('(' (expr (',' expr)*)? ')')?
 primary ::= number|identifier|('(' expr ')')
 factor ::= primary ('^' primary)?
 term ::= factor (('*'|'/'|'%') factor)*
@@ -21,24 +23,29 @@ expr ::= ('+'|'-')? term (('+'|'-') term)*
 */
 
 using value_t = double;
-std::map<std::string, value_t> env;
+using function_t = std::function<value_t(const std::vector<value_t>&)>;
 
-
-void trace(std::string msg, char token) {
-    std::cout << "trace: " << msg << " (" << token << ")" << std::endl;
-}
-
+std::map<std::string, value_t> constants;
+std::map<std::string, value_t> variables;
+std::map<std::string, function_t> functions;
 
 class input_t {
     public:
         input_t(std::istream& input) : input_(input){
             advance();
         }
+        bool eof() {
+            return input_.eof();
+        }
         char next() {
             return next_;
         }
         void advance(){
             next_ = input_.get();
+        }
+        void skip() {
+            while (!eof() && isblank(next()))
+                advance();
         }
     private:
         std::istream& input_;
@@ -48,20 +55,24 @@ class input_t {
 value_t parse_expr(input_t& input);
 
 value_t parse_number(input_t& input) {
-    value_t result;
+    value_t result = 0;
     int exp = 0;
     bool negexp = false;
 
     char chr = input.next();
-    while (isdigit(chr)) {
+    if (input.eof() || !isdigit(chr))
+        throw std::invalid_argument("number: expected digit, got "+chr);
+    while (!input.eof() && isdigit(chr)) {
         result = result*10 + static_cast<int>(chr-'0');
         input.advance();
         chr = input.next();
     }
-    if (chr == '.') {
+    if (!input.eof() && (chr == '.')) {
         input.advance();
         chr = input.next();
-        while (isdigit(chr)) {
+        if (input.eof() || !isdigit(chr))
+            throw std::invalid_argument("number: expected digit after decimal point, got "+chr);
+        while (!input.eof() && isdigit(chr)) {
             result = result*10 + static_cast<int>(chr-'0');
             exp--;
             input.advance();
@@ -70,7 +81,7 @@ value_t parse_number(input_t& input) {
         result *= pow(10,exp);
         exp = 0;
     }
-    if (chr == 'e') {
+    if (!input.eof() && (chr == 'e')) {
         input.advance();
         chr = input.next();
         if (chr == '+') {
@@ -78,9 +89,10 @@ value_t parse_number(input_t& input) {
         } else if (chr == '-') {
             negexp = true;
             input.advance();
-        }
+        } else if (input.eof() || !isdigit(chr))
+            throw std::invalid_argument("number: expected sign or digit after exponent indicator, got "+chr);
         chr = input.next();
-        while (isdigit(chr)) {
+        while (!input.eof() && isdigit(chr)) {
             exp = exp*10 + static_cast<int>(chr-'0');
             input.advance();
             chr = input.next();
@@ -89,66 +101,102 @@ value_t parse_number(input_t& input) {
             exp = -exp;
         result *= pow(10, exp);
     }
+    input.skip();
     return result;
 }
 value_t parse_identifier(input_t& input) {
     std::string name;
 
     char chr = input.next();
-    if (!isalpha(chr))
-        throw std::invalid_argument("parse error: identifier: expected alpha, got "+chr);
-    while (isalnum(chr) || (chr == '_')) {
+    if (input.eof() || !isalpha(chr))
+        throw std::invalid_argument("identifier: expected alpha, got "+chr);
+    while (!input.eof() && (isalnum(chr) || (chr == '_'))) {
         name.push_back(chr);
         input.advance();
         chr = input.next();
     }
 
-    decltype(env)::iterator ident = env.find(name);
-    if (ident == env.end())
-        throw std::invalid_argument("execution error: unknown identifier: "+name);
-    
-    return ident->second;
+    if (!input.eof() && (chr == '(')) {
+        std::vector<value_t> args;
+        input.advance();
+        chr = input.next();
+        if (!input.eof() && (chr != ')')) {
+            args.push_back(parse_expr(input));
+            chr = input.next();
+            while (!input.eof() && (chr == ',')) {
+                input.advance();
+                input.skip();
+                args.push_back(parse_expr(input));
+                chr = input.next();
+            }
+        }
+        if (!input.eof() && (chr != ')')) 
+            throw std::invalid_argument("call: expected ), got "+chr);
+        input.advance();
+
+        decltype(functions)::iterator function = functions.find(name);
+        if (function != functions.end())
+            return function->second(args);
+        throw std::runtime_error("identifier: undefined function: "+name);
+    } else {
+        decltype(constants)::iterator constant = constants.find(name);
+        if (constant != constants.end())
+            return constant->second;
+        decltype(variables)::iterator variable = variables.find(name);
+        if (variable != variables.end()) 
+            return variable->second;
+        throw std::runtime_error("identifier: undefined identifier: "+name);
+    }
+    input.skip();
 }
 value_t parse_primary(input_t& input) {
-    value_t result;
+    value_t result = 0;
     char chr = input.next();
-    if (isdigit(chr)) {
+    if (input.eof()) {
+        throw std::invalid_argument("primary: input is empty");
+    } else if (isdigit(chr)) {
         result = parse_number(input);
     } else if (isalpha(chr)) {
         result = parse_identifier(input);
     }  else if (chr == '(') {
-        input.advance();
+        input.advance(); 
         result = parse_expr(input);
+        input.skip();
         chr = input.next();
-        if (chr != ')')
-            throw std::invalid_argument("parse error: primary: expected ')', got "+chr);
+        if (input.eof() || (chr != ')'))
+            throw std::invalid_argument("primary: expected ')', got "+chr);
         input.advance();
+        input.skip();
+    } else {
+        throw std::invalid_argument("primary: expected number, identifier or (expression)");
     }
     return result;
 }
 value_t parse_factor(input_t& input) {
     value_t result = parse_primary(input);
-
     char chr = input.next();
-    if (chr == '^') {
+    if (!input.eof() && (chr == '^')) {
         input.advance();
+        input.skip();
         result = pow(result, parse_primary(input)); 
     }
     return result;
 }
 value_t parse_term(input_t& input) {
     value_t result = parse_factor(input);
-
     char chr = input.next();
-    while ((chr == '*') || (chr == '/') || (chr == '%')) {
+    while (!input.eof() && ((chr == '*') || (chr == '/') || (chr == '%'))) {
         if (chr == '*') {
             input.advance();
+            input.skip();
             result *= parse_factor(input);
         } else if (chr == '/') {
             input.advance();
+            input.skip();
             result /= parse_factor(input);
         } else if (chr == '%') {
             input.advance();
+            input.skip();
             result  = fmod(result, parse_factor(input));
         }
         chr = input.next();
@@ -158,12 +206,17 @@ value_t parse_term(input_t& input) {
 value_t parse_expr(input_t& input) {
     bool negate = false;
 
+    input.skip();
     char chr = input.next();
-    if (chr == '+') {
+    if (input.eof()) {
+        throw std::invalid_argument("expr: input is empty");
+    } else if (chr == '+') {
         input.advance();
+        input.skip();
     } else if (chr == '-') {
         negate = true;
         input.advance();
+        input.skip();
     }
 
     value_t result = parse_term(input);
@@ -171,12 +224,14 @@ value_t parse_expr(input_t& input) {
         result = -result;
     
     chr = input.next();
-    while ((chr == '+') || (chr == '-')) {
+    while (!input.eof() && ((chr == '+') || (chr == '-'))) {
         if (chr == '+') {
             input.advance();
+            input.skip();
             result += parse_term(input);
         } else if (chr == '-') {
             input.advance();
+            input.skip();
             result -= parse_term(input);
         }
         chr = input.next();
@@ -185,11 +240,53 @@ value_t parse_expr(input_t& input) {
     return result;
 }
 
-
 void setup() {
-    env["pi"] = 3.1415926535898;
+    constants["pi"] = 3.1415926535898;
+    constants["e"]  = 2.7182818284590;
+    functions["abs"] = [](std::vector<value_t> args)->value_t { 
+        if (args.size()!=1)
+            throw std::runtime_error("abs: wrong number of arguments"); 
+        return abs(args[0]); 
+    };
+    functions["pow"] = [](std::vector<value_t> args)->value_t { 
+        if (args.size()!=2)
+            throw std::runtime_error("sqrt: wrong number of arguments"); 
+        return pow(args[0], args[1]); 
+    };
+    functions["sqrt"] = [](std::vector<value_t> args)->value_t { 
+        if (args.size()!=1)
+            throw std::runtime_error("sqrt: wrong number of arguments"); 
+        return sqrt(args[0]); 
+    };
+    functions["exp"] = [](std::vector<value_t> args)->value_t { 
+        if (args.size()!=1)
+            throw std::runtime_error("exp: wrong number of arguments"); 
+        return exp(args[0]); 
+    };
+    functions["log"] = [](std::vector<value_t> args)->value_t { 
+        if (args.size() == 1)
+            return log(args[0]);
+        else if (args.size() == 2)
+            return log(args[0])/log(args[1]);
+        else
+            throw std::runtime_error("log: wrong number of arguments"); 
+    };
+    functions["sin"] = [](std::vector<value_t> args)->value_t { 
+        if (args.size()!=1)
+            throw std::runtime_error("sin: wrong number of arguments"); 
+        return sin(args[0]); 
+    };
+    functions["cos"] = [](std::vector<value_t> args)->value_t { 
+        if (args.size()!=1) 
+            throw std::runtime_error("cos: wrong number of arguments"); 
+        return cos(args[0]); 
+    };
+    functions["tan"] = [](std::vector<value_t> args)->value_t { 
+        if (args.size()!=1) 
+            throw std::runtime_error("tan: wrong number of arguments"); 
+        return tan(args[0]); 
+    };
 }
-
 int main(int argc, char*argv[]) {
     setup();
     while (true) {
@@ -199,7 +296,9 @@ int main(int argc, char*argv[]) {
             value_t result = parse_expr(input);
             std::cout << "= " << result << std::endl;
         } catch (const std::invalid_argument& error) {
-            std::cout << error.what() << std::endl;
+            std::cout << "parse error: " << error.what() << std::endl;
+        } catch (const std::runtime_error& error) {
+            std::cout << "execution error: " << error.what() << std::endl;
         }
     }
 }
